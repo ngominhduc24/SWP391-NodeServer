303d88bc7f1f512675c349036e29081f8565f05b
Minh Duc <ngominhduc100@gmail.com>
2023-10-09 20:49:01 +0700

    feat: pupperteer get all student class

---
+++ src/controller/puppeteer.js
@@ -0,0 +1,85 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const userDataDir = 'C:\\Users\\md8qt\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1';
+
+(async () => {
+
+    // Mở trình duyệt mới và tới trang của kenh14
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        userDataDir: 'C:\\Users\\md8qt\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1\\Cache\\Cache_Data',
+    });
+    const page = await browser.newPage();
+    await page.goto('https://fap.fpt.edu.vn/Default.aspx');
+
+    // Chạy đoạn JavaScript trong hàm này, đưa kết quả vào biến article
+    const articles = await page.evaluate(() => {
+        function Login() {
+            const footer = document.getElementById("cssTable");
+            if (!footer) return;
+            const campusSelection = window.document.querySelector("#ctl00_mainContent_ddlCampus");
+            if (!campusSelection) return;
+            campusSelection.value = "3";
+            footer.insertAdjacentHTML(
+                "beforeend",
+                '<h5 id="pointer_button" style="text-align: center;display:none; font-weight: bold;" onclick=" \n \
+            const loginButton = document.querySelector(\'#ctl00_mainContent_btnLogin\'); \n \
+            if (loginButton) \n \
+              loginButton.click(); \n \
+          "></h5>'
+            );
+            const loginButton = document.querySelector("#pointer_button");
+            if (!loginButton) return;
+            loginButton.click();
+        }
+
+        Login();
+
+        function gacGetLoginElements() {
+            return document.querySelectorAll('[data-identifier]');
+        }
+
+        async function gacPerformLogin(email) {
+            setTimeout(function () {
+                let loginElements = gacGetLoginElements();
+                for (let i = 0; i < loginElements.length; i++) {
+                    let el = loginElements[i];
+                    let elEmail = el.getAttribute("data-identifier");
+                    console.log(elEmail);
+                    if (elEmail === email) {
+                        el.click();
+                    }
+                }
+            }, 1500); // Wait for 1 second
+        }
+
+        gacPerformLogin("ducnmhe173177@fpt.edu.vn");
+
+    });
+
+
+    await new Promise(r => setTimeout(r, 1000));
+    const page2 = await browser.newPage();
+    await page2.goto('https://fap.fpt.edu.vn/Course/Groups.aspx?group=38448');
+
+    let data = await page2.evaluate(() => {
+
+        let table = document.getElementById('id');
+        let rows = table.querySelectorAll("tr");
+        let data = [];
+        for (let i = 0; i < rows.length; i++) {
+            let row = [],
+                cols = rows[i].querySelectorAll("td, th");
+            for (let j = 0; j < cols.length; j++) {
+                row.push(cols[j].innerText);
+            }
+            data.push(row);
+        }
+        return data;
+    });
+
+    console.log(data);
+    await browser.close();
+})();

c1aed6898084ce2ab074046c53dde7f28c427b62
Minh Duc <ngominhduc100@gmail.com>
2023-10-11 12:09:49 +0700

    pass param class

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,12 +1,56 @@
 const myFunction = require('./puppeteer');
 
 let getData = async (req, res) => {
-  const hrefCrawl = 'https://fap.fpt.edu.vn/Course/Groups.aspx?group=38482';
-  let data = await myFunction(hrefCrawl);
+  if (!req.query.subject && !req.query.class) {
+    return res.status(400).json({
+      status: 400,
+      message: 'Bad request',
+    });
+  }
 
-  return res.status(200).json(data);
+  let database;
+  const fs = require('fs');
+
+  fs.readFile('./database.json', 'utf8', (error, data) => {
+    if (error) {
+      return res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+      });
+    }
+    database = JSON.parse(data);
+
+
+    let hrefCrawl = '';
+    const myClass = req.query.class;
+    for (const element of database) {
+      if (element.name.toLowerCase() == myClass) {
+        hrefCrawl = element.link;
+        break; // Stop searching once you find a matching element
+      }
+    }
+
+    if (hrefCrawl == '') {
+      return res.status(400).json({
+        status: 400,
+        message: 'Bad request',
+      });
+    }
+
+
+    myFunction(hrefCrawl)
+      .then(data => {
+        return res.status(200).json(data);
+      })
+      .catch(error => {
+        console.log(error);
+        return res.status(500).json({
+          status: 500,
+          message: 'Internal server error',
+        });
+      });
+  });
 };
-
 
 module.exports = {
   getData,
